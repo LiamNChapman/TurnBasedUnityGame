9551b61ce84f9ca4bc2ba66f9a30aeaab690da3b
Calum Hunter <hunca608@student.otago.ac.nz>
2019-01-22 15:39:37 +1300

    added player states that can be toggled through when hitting a button. made the button set to not active when the state is not the players turn

---
+++ Assets/Prototypes/Turns/Scripts/LevelManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public enum TurnStates {
+		PLAYER,
+		PLAYERMOVE,
+		ENEMYMOVE
+
+	}
+	public GameObject upButton;
+	public GameObject downButton;
+	public GameObject leftButton;
+	public GameObject rightButton;
+	TurnStates currentState;
+	// Use this for initialization
+	void Start () {
+		currentState = TurnStates.PLAYER;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(currentState);
+		switch(currentState) {
+			case(TurnStates.PLAYER):
+				break;
+			case(TurnStates.PLAYERMOVE):
+				nextState();
+				break;
+			case(TurnStates.ENEMYMOVE):
+				nextState();
+				break;
+		}	
+	}
+	public void nextState() {
+		if(currentState == TurnStates.PLAYER) {
+			currentState = TurnStates.PLAYERMOVE;
+			button.SetActive(false);
+		} else if(currentState == TurnStates.PLAYERMOVE) {
+			currentState = TurnStates.ENEMYMOVE;
+		} else {
+			currentState = TurnStates.PLAYER;
+			button.SetActive(true);
+		}
+	}
+
+	void playerTurnSetup() {
+
+	}
+}
