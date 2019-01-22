d97927ed87daba4cab9e95ccc307f4831b1554bc
Jonathan Marshall <marjo989@student.otago.ac.nz>
2019-01-23 12:41:38 +1300

    GridMovement prototype now complete, tile by tile movement while also keeping the player within the bounds. More info on wiki

--- Prototypes/Prototyping/Assets/Scripts/Player.cs
+++ Prototypes/Prototyping/Assets/Scripts/Player.cs
@@ -6,43 +6,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour {
 
+	public bool exitPath = false;
+
 	public int prev;
-	public float speed = 1.0f;
 	
 	//Update is called once per frame
 	//If one of the four directional buttons pushed, determine which one, set the movePosition,
 	// and set move to true so that the fixedUpdate method will move the player.
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+	void FixedUpdate () {
+		if(Input.GetKeyDown(KeyCode.A)){
 			transform.position += Vector3.left;
 			prev = 1;
-			LevelManager.nextState();
-		}
-		
-		if(Input.GetKeyDown(KeyCode.RightArrow)){
-			transform.position += Vector3.right ;
+
+			//We may want to only change state if we're sure the player is in a different place 
+			//As in it depends on how we handle collsions with the path collider
+			//LevelManager.nextState();
+		}else if(Input.GetKeyDown(KeyCode.D)){
+			transform.position += Vector3.right;
 			prev = 2;
-			LevelManager.nextState();
-		}
-		
-		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			//LevelManager.nextState();
+		}else if(Input.GetKeyDown(KeyCode.W)){
 			transform.position += Vector3.up;
 			prev = 3;
-			LevelManager.nextState();
-		}
-		
-		if(Input.GetKeyDown(KeyCode.DownArrow)){
+			//LevelManager.nextState();
+		}else if(Input.GetKeyDown(KeyCode.S)){
 			transform.position += Vector3.down;
 			prev = 4;
-			LevelManager.nextState();
+			//LevelManager.nextState();
 		}
-	}
 
-	void OnTriggerEnter2D(Collider2D other){
-		if(other.gameObject.tag == "Path"){
+		if(exitPath){
 			if(prev == 1){
 				transform.position += Vector3.right;
 			}else if(prev == 2){
@@ -53,6 +50,14 @@ public class Player : MonoBehaviour {
 				transform.position += Vector3.up;
 			}
 		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		exitPath = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		exitPath = false;
 	}
 
 }
