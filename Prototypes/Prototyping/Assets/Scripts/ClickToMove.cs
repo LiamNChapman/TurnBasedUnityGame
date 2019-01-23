94b3c9261b22412c495514c862a74a04cf3cd39c
Liam Chapman <chali172@student.otago.ac.nz>
2019-01-23 17:36:23 +1300

    Player movement only limited to one tile at a time.

--- Prototypes/Prototyping/Assets/Scripts/ClickToMove.cs
+++ Prototypes/Prototyping/Assets/Scripts/ClickToMove.cs
@@ -1,7 +1,14 @@
-﻿using System.Collections;
+﻿/*
+	Created by Liam Chapman
+	23rd Jan 2019
+	Player movement by clicking, only one tile at a time.
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System;
 
 public class ClickToMove : MonoBehaviour {
 
@@ -16,19 +23,30 @@ public class ClickToMove : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		
-			
+		
 		if(Input.GetMouseButtonDown(0)) {
-			Debug.Log("player start position: " + this.transform.position);
+			//get the coordinates from the mouse click.
 			Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         	Vector3Int coordinate = grid.WorldToCell(mouseWorldPos);
-			if(tilemap.GetTile(coordinate).name != "NonPath"){
-				Vector3 newPosition = (Vector3)coordinate;
-				newPosition.x += 0.5f;
-				newPosition.y += 0.5f;
-				this.transform.position = newPosition;
+
+			// bool values to check if the distance the player has clicked to move to
+			// is only 1 tile either up or down, or left or right.
+			bool leftAndRight = Math.Abs((this.transform.position.x-0.5f) - coordinate.x) == 1;
+			bool upAndDown = Math.Abs((this.transform.position.y-0.5f) - coordinate.y) == 1;
+
+			// Check if the tile isn't a path.
+			if(tilemap.GetTile(coordinate).name != "NonPath" ){
+
+				//Check to make sure the player doesnt move diagonal.
+				if((upAndDown && !leftAndRight) || (!upAndDown && leftAndRight)){
+					Vector3 newPosition = (Vector3)coordinate;
+
+					// Shift the players character to the center of the tile.
+					newPosition.x += 0.5f;
+					newPosition.y += 0.5f;
+					this.transform.position = newPosition;
+				}
 			}
-        	Debug.Log("click: " + coordinate);
-			Debug.Log("New Player Position: " + this.transform.position);
 		}
 	}
 }
