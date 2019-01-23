e02c6f8130175f149b054ae670cb2de2434a4c5e
Calum Hunter <hunca608@student.otago.ac.nz>
2019-01-23 13:11:01 +1300

    added a script that gets the coordinates of a tile when clicked

--- Prototypes/Prototyping/Assets/Turns/Scripts/LevelManager.cs
+++ Prototypes/Prototyping/Assets/Turns/Scripts/LevelManager.cs
@@ -20,7 +20,7 @@ public class LevelManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(currentState + " " + turnCount);
+		//Debug.Log(currentState + " " + turnCount);
 		if(currentState == TurnStates.ENEMYMOVE) {
 			nextState();
 		}
