822e3446483ce17c99f3f54f64efd7840e0cad50
John McWatters <mcwjo648@student.otago.ac.nz>
2019-01-25 11:21:50 +1300

    added los functionality (barebones) and ranger

---
+++ Prototypes/Prototyping/Assets/Scripts/Bezerker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bezerker : MonoBehaviour
+{
+
+    public bool angry = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (TurnManagerLOS.currentState == TurnManagerLOS.TurnStates.ENEMYMOVE)
+        {
+
+            TurnManagerLOS.nextState();
+        }
+    }
+}
--- Prototypes/Prototyping/Assets/Scripts/Ranger.cs
+++ Prototypes/Prototyping/Assets/Scripts/Ranger.cs
@@ -1,16 +1,65 @@
-﻿using System.Collections;
+﻿/*
+    Created by Joe Marshall
+    Jan 23rd, 2019
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ranger : MonoBehaviour {
+public class Ranger : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Only move if it is the enemy turn state
+    void Update()
+    {
+        //if(TurnManagerLOS.currentState == TurnManagerLOS.TurnStates.ENEMYMOVE){
+        
+        this.transform.Rotate(0, 0, 90);
+        TurnManagerLOS.nextState();
+        //}
+    }
+
+    /*
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (vertical)
+        {
+            //If the Sprite exited at the top of a path
+            if (direction == 1)
+            {
+                //Move it back onto the path
+                this.transform.position += Vector3.down;
+                //Set it to move downwards
+                direction = 0;
+                //Move down one space
+                this.Move();
+            }
+            else
+            {
+                this.transform.position += Vector3.up;
+                direction = 1;
+                this.Move();
+            }
+        }
+        else
+        {
+            //If the sprite exited on the right of a path
+            if (direction == 1)
+            {
+                //Move back onto path
+                this.transform.position += Vector3.left;
+                //Set to move left
+                direction = 0;
+                //Move
+                this.Move();
+            }
+            else
+            {
+                this.transform.position += Vector3.right;
+                direction = 1;
+                this.Move();
+            }
+        }
+    }*/
 }
