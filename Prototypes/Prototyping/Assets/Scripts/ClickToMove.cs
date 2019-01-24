7701a413e53e610ec0c46b5b361b5e1e778abbd4
Liam Chapman <chali172@student.otago.ac.nz>
2019-01-24 16:52:00 +1300

    Fixed bug with ClickToMove

--- Prototypes/Prototyping/Assets/Scripts/ClickToMove.cs
+++ Prototypes/Prototyping/Assets/Scripts/ClickToMove.cs
@@ -33,12 +33,14 @@ public class ClickToMove : MonoBehaviour {
 			// is only 1 tile either up or down, or left or right.
 			bool leftAndRight = Math.Abs((this.transform.position.x-0.5f) - coordinate.x) == 1;
 			bool upAndDown = Math.Abs((this.transform.position.y-0.5f) - coordinate.y) == 1;
+			bool leftAndRight2 = Math.Abs((this.transform.position.x-0.5f) - coordinate.x) == 0;
+			bool upAndDown2 = Math.Abs((this.transform.position.y-0.5f) - coordinate.y) == 0;
 
 			// Check if the tile isn't a path.
 			if(tilemap.GetTile(coordinate).name != "NonPath" ){
 
 				//Check to make sure the player doesnt move diagonal.
-				if((upAndDown && !leftAndRight) || (!upAndDown && leftAndRight)){
+				if((upAndDown && leftAndRight2) || (leftAndRight && upAndDown2)){
 					Vector3 newPosition = (Vector3)coordinate;
 
 					// Shift the players character to the center of the tile.
