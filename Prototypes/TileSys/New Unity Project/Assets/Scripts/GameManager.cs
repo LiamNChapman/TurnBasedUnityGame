f8a1e4e316523dec9f97fd68129d681f15653ccb
John McWatters <mcwjo648@student.otago.ac.nz>
2019-01-28 16:14:40 +1300

    tile and colision system project

---
+++ Prototypes/TileSys/New Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject[] enemies;
+    public GameObject player;
+    public bool playerTurn;
+
+    void Start()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        player = GameObject.Find("Player");
+        playerTurn = true;
+        //Turn();
+    }
+
+    void Update()
+    {
+        player.GetComponent<Character>().LocalUpdate();
+        foreach (GameObject go in enemies)
+        {
+            go.GetComponent<Character>().LocalUpdate();
+            if (go.GetComponent<Character>().xLoc == player.GetComponent<Character>().xLoc && go.GetComponent<Character>().yLoc == player.GetComponent<Character>().yLoc)
+            {
+                Debug.Log("death");
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+    /*
+    void Turn () {
+        if (playerTurn){
+
+        }
+    }*/
+}
+
+
