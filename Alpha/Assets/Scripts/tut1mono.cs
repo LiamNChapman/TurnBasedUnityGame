b3bb014d3e5dfc260f550bebddd7b27f7da0059b
Jonathan Marshall <marjo989@student.otago.ac.nz>
2019-02-08 15:25:33 +1300

    Tutorial 1 complete

--- Alpha/Assets/Scripts/tut1mono.cs
+++ Alpha/Assets/Scripts/tut1mono.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class tut1mono : MonoBehaviour {
 
-	public GameObject Player;
 	public GameObject Ranger;
 	public GameObject TurnManager;
 
 	public void GameStart(){
 		TurnManager.GetComponent<TurnManager>().enabled = true;
-		Player.GetComponent<Player>().enabled = true;
-		Player.GetComponent<testingTileHighlights>().enabled = true;
 		Ranger.GetComponent<Ranger>().enabled = true;
 	}
 }
