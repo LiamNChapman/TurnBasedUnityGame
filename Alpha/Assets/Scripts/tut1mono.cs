c92bde9e705a17c6eacc3e69f5f093a28ecd3ca6
Jonathan Marshall <marjo989@student.otago.ac.nz>
2019-02-08 15:03:04 +1300

    Tutorial level 1 now has monologue

---
+++ Alpha/Assets/Scripts/tut1mono.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tut1mono : MonoBehaviour {
+
+	public GameObject Player;
+	public GameObject Ranger;
+
+	public void GameStart(){
+		Player.GetComponent<Player>().enabled = true;
+		Ranger.GetComponent<Ranger>().enabled = true;
+	}
+}
