f477d0d42bbbb8a9f3b807fa5bc988c5332289ba
Jonathan Marshall <marjo989@student.otago.ac.nz>
2019-02-10 18:28:19 +1300

    The gate animations exist but are fiddly and won't be (fully) working for the beta.

--- Alpha/Assets/Scripts/Gate.cs
+++ Alpha/Assets/Scripts/Gate.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class Gate : MonoBehaviour {
 	public bool open = false;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+	GameObject OpenInstance;
+	GameObject CloseInstance;
+	public GameObject GateClose;
+	public GameObject GateOpen;
 	
 	// Update is called once per frame
 	void Update () {
 		if(open){
+			//CloseInstance = Instantiate(GateOpen, transform.position, Quaternion.identity);
+			//Destroy(CloseInstance);
 			gameObject.SetActive(false);
 		}
 		if (!open){
+			//CloseInstance = Instantiate(GateClose, transform.position, Quaternion.identity);
+			//Destroy(CloseInstance);
 			gameObject.SetActive(true);
 		}
 	}
