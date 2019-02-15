cca1197a63d3407b6b81e3f32a17a7b41ddebf1b
John McWatters <mcwjo648@student.otago.ac.nz>
2019-02-15 15:35:14 +1300

    end scene with art and animation

---
+++ Alpha/Assets/Scripts/Animate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour {
+
+    public Sprite[] spriteList;
+    int count = 0;
+    int delay = 0;
+    // Update is called once per frame
+
+    void Update () {
+        if (delay > 20) {
+            this.GetComponent<SpriteRenderer>().sprite = spriteList[count];
+            count++;
+            if (count == 4)
+            {
+                count = 0;
+            }
+            delay = 0;
+        } else {
+            delay++;
+        }
+	}
+}
